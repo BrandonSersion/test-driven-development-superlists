94f5041396a0bb06b43e7d90f0cec8d34ce274bd
BrandonSersion <addingavocado@gmail.com>
2018-03-08 17:43:08 -0500

    switched back to development server / comment out production server stuff

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,10 +9,14 @@ MAX_WAIT = 10
 class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
+        #development server
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')  
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+
+        #production server
+        # self.browser = webdriver.Firefox()
+        # staging_server = os.environ.get('STAGING_SERVER')  
+        # if staging_server:
+        #     self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
