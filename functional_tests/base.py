8e2ad0e7241de3d69faec41ec633a3811aab52ec
BrandonSersion <addingavocado@gmail.com>
2018-03-08 18:45:44 -0500

    Split unit tests into two files

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -33,3 +33,13 @@ class FunctionalTest(StaticLiveServerTestCase):
             except (AssertionError, WebDriverException) as e:  
                 if time.time() - start_time > MAX_WAIT:  
                     raise e  
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()  
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
